48e646d50c6df28b7df356eccb926f395141346a
Andre Zimin <130975402+andrzimin72@users.noreply.github.com>
2026-08-21 18:10:42 +0800

    Add files via upload

--- swarms/patterns/__init__.py
+++ swarms/patterns/__init__.py
@@ -1,1 +1,13 @@
+"""
+Pre-built Swarm Patterns
+"""
+from .file_analysis_swarm import FileAnalysisSwarm
+from .well_performance_swarm import WellPerformanceSwarm
+from .qa_swarm import QASwarm
+from .pad_analysis_swarm import PadAnalysisSwarm
+from .end_of_well_swarm import EndOfWellSwarm
 
+__all__ = [
+    "FileAnalysisSwarm", "WellPerformanceSwarm", 
+    "QASwarm", "PadAnalysisSwarm", "EndOfWellSwarm"
+]
