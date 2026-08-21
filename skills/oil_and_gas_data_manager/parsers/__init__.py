419b88be0fb5766ee0c62b57bb0438925407404b
Andre Zimin <130975402+andrzimin72@users.noreply.github.com>
2026-08-21 17:55:00 +0800

    Add files via upload

--- skills/oil_and_gas_data_manager/parsers/__init__.py
+++ skills/oil_and_gas_data_manager/parsers/__init__.py
@@ -1,1 +1,14 @@
+"""
+File Format Parsers
+"""
+from .las_parser import LasParser
+from .dlis_parser import DlisParser
+from .pdf_reports import PdfReportParser
+from .csv_parser import CsvParser
+from .spreadsheet_parser import SpreadsheetParser
+from .docx_parser import DocxParser
 
+__all__ = [
+    "LasParser", "DlisParser", "PdfReportParser", 
+    "CsvParser", "SpreadsheetParser", "DocxParser"
+]
